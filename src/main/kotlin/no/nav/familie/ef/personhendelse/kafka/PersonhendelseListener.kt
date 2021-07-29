84457430cd8d146b5f25236b031b90bffa3e0529
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-07-29 14:09:30 +0200

    Read last message

--- src/main/kotlin/no/nav/familie/ef/personhendelse/kafka/PersonhendelseListener.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/kafka/PersonhendelseListener.kt
@@ -30,7 +30,7 @@ class PersonhendelseListener(val dodsfallHandler: DodsfallHandler) {
         topicPartitions = [TopicPartition(
             topic = "aapen-person-pdl-leesah-v1",
             partitionOffsets = [PartitionOffset(
-                initialOffset = "0", partition = "0"
+                initialOffset = "4582464", partition = "0"
             )]
         )]
     )
