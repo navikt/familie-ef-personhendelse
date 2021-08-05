d277790d05473bf20f5537cf385bed3fca844a8b
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-08-05 11:12:58 +0200

    Egendefinert request for henting av azure access token

---
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/common/AzureClient.kt
@@ -0,0 +1,46 @@
+package no.nav.familie.ef.personhendelse.common
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.postForEntity
+
+
+@Component
+class AzureClient(
+    @Value("\${AZURE_OPENID_CONFIG_TOKEN_ENDPOINT}")
+    private val url: String,
+    @Value("\${AZURE_APP_CLIENT_ID}")
+    private val clientId: String,
+    @Value("\${EF_SAK_SCOPE}")
+    private val efSakScope: String,
+    @Value("\${AZURE_APP_CLIENT_SECRET}")
+    private val clientSecret: String,
+) {
+
+    fun hentToken(): String {
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
+
+        val map: MultiValueMap<String, String> = LinkedMultiValueMap()
+        map.add("client_id", clientId)
+        map.add("scope", efSakScope)
+        map.add("client_secret", clientSecret)
+        map.add("grant_type", "client_credentials")
+
+
+        val request = HttpEntity<MultiValueMap<String, String>>(map, headers)
+
+        val restTemplate = RestTemplate()
+        val response = restTemplate.postForEntity<Token>(url, request)
+        if (response.body == null) {
+            throw Exception("Fikk ikke hentet ut token")
+        }
+        return response.body!!.token
+    }
+}
