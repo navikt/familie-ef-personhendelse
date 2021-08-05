79411e421ae6976e8f92a8ac87d057feb89e414c
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-08-05 08:46:52 +0200

    Override OAuth2HttpClient

--- src/main/kotlin/no/nav/familie/ef/personhendelse/configuration/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/configuration/ApplicationConfig.kt
@@ -1,12 +1,19 @@
 package no.nav.familie.ef.personhendelse.configuration
 
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import no.nav.familie.http.config.RestTemplateAzure
 import no.nav.familie.kontrakter.felles.objectMapper
+import no.nav.familie.log.filter.LogFilter
+import no.nav.familie.log.filter.RequestTimeFilter
+import no.nav.security.token.support.client.core.http.OAuth2HttpClient
+import no.nav.security.token.support.client.spring.oauth2.DefaultOAuth2HttpClient
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Primary
@@ -23,6 +30,29 @@ import java.time.temporal.ChronoUnit
 @EnableOAuth2Client(cacheEnabled = true)
 class ApplicationConfig {
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @Bean
+    fun kotlinModule(): KotlinModule = KotlinModule()
+
+    @Bean
+    fun logFilter(): FilterRegistrationBean<LogFilter> {
+        logger.info("Registering LogFilter filter")
+        val filterRegistration = FilterRegistrationBean<LogFilter>()
+        filterRegistration.filter = LogFilter()
+        filterRegistration.order = 1
+        return filterRegistration
+    }
+
+    @Bean
+    fun requestTimeFilter(): FilterRegistrationBean<RequestTimeFilter> {
+        logger.info("Registering RequestTimeFilter filter")
+        val filterRegistration = FilterRegistrationBean<RequestTimeFilter>()
+        filterRegistration.filter = RequestTimeFilter()
+        filterRegistration.order = 2
+        return filterRegistration
+    }
+
     /**
      * Overskrever felles sin som bruker proxy, som ikke skal brukes på gcp
      */
@@ -34,5 +64,18 @@ class ApplicationConfig {
             .setConnectTimeout(Duration.of(2, ChronoUnit.SECONDS))
             .setReadTimeout(Duration.of(30, ChronoUnit.SECONDS))
             .messageConverters(listOf(jackson2HttpMessageConverter) + RestTemplate().messageConverters)
+    }
+
+    /**
+     * Overskrever OAuth2HttpClient som settes opp i token-support som ikke kan få med objectMapper fra felles
+     * pga .setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE)
+     * og [OAuth2AccessTokenResponse] som burde settes med setters, då feltnavn heter noe annet enn feltet i json
+     */
+    @Bean
+    @Primary
+    fun oAuth2HttpClient(): OAuth2HttpClient {
+        return DefaultOAuth2HttpClient(RestTemplateBuilder()
+            .setConnectTimeout(Duration.of(2, ChronoUnit.SECONDS))
+            .setReadTimeout(Duration.of(4, ChronoUnit.SECONDS)))
     }
 }
