8ed2e72cb831495625f1f5a71ae36c21de48e278
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-08-13 11:27:46 +0200

    Opprett oppgave ved første dødsfall hendelse

--- src/main/kotlin/no/nav/familie/ef/personhendelse/dodsfall/DodsfallHandler.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/dodsfall/DodsfallHandler.kt
@@ -4,10 +4,17 @@ import no.nav.familie.ef.personhendelse.oppgave.OppgaveClient
 import no.nav.familie.ef.personhendelse.pdl.PdlClient
 import no.nav.familie.ef.personhendelse.sak.SakClient
 import no.nav.familie.kontrakter.ef.felles.StønadType
+import no.nav.familie.kontrakter.felles.Behandlingstema
+import no.nav.familie.kontrakter.felles.Tema
+import no.nav.familie.kontrakter.felles.oppgave.IdentGruppe
+import no.nav.familie.kontrakter.felles.oppgave.OppgaveIdentV2
+import no.nav.familie.kontrakter.felles.oppgave.Oppgavetype
+import no.nav.familie.kontrakter.felles.oppgave.OpprettOppgaveRequest
 import no.nav.person.pdl.leesah.Personhendelse
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
 import java.util.*
 
 @Component
@@ -20,6 +27,8 @@ class DodsfallHandler(
     private val secureLogger = LoggerFactory.getLogger("secureLogger")
     private val logger = LoggerFactory.getLogger(javaClass)
 
+    private var lagdOppgave = false
+
     @Transactional
     fun handleDodsfallHendelse(personhendelse: Personhendelse) {
         val personIdent = personhendelse.personidenter.map { it.toString() }.first()
@@ -29,27 +38,27 @@ class DodsfallHandler(
         val person = pdlClient.hentPerson(personIdent, UUID.randomUUID().toString())
         secureLogger.info("Person hentet: ${person.data?.hentPerson?.forelderBarnRelasjon}")
         val finnesBehandlingForPerson = sakClient.finnesBehandlingForPerson(personIdent, StønadType.OVERGANGSSTØNAD)
-        secureLogger.info("Finnes behandling for person: $finnesBehandlingForPerson")
         logger.info("Finnes behandling for person: $finnesBehandlingForPerson")
         if (finnesBehandlingForPerson) {
             secureLogger.info("Oppgave opprettes for person: $personIdent")
-            /*
-            val opprettOppgaveRequest =
-                OpprettOppgaveRequest(
-                    ident = OppgaveIdentV2(ident = personIdent, gruppe = IdentGruppe.FOLKEREGISTERIDENT),
-                    saksId = null,
-                    tema = Tema.ENF,
-                    oppgavetype = Oppgavetype.VurderHenvendelse,
-                    fristFerdigstillelse = LocalDate.now(),
-                    beskrivelse = "Saken ligger i ny løsning. Opprettet som følge av personhendelse",
-                    enhetsnummer = null,
-                    behandlingstema = Behandlingstema.Overgangsstønad.value,
-                    tilordnetRessurs = null,
-                    behandlesAvApplikasjon = "familie-ef-sak"
-                )
-            oppgaveClient.opprettOppgave(opprettOppgaveRequest)
-
-             */
+            if (!lagdOppgave) {
+                val opprettOppgaveRequest =
+                    OpprettOppgaveRequest(
+                        ident = OppgaveIdentV2(ident = personIdent, gruppe = IdentGruppe.FOLKEREGISTERIDENT),
+                        saksId = null,
+                        tema = Tema.ENF,
+                        oppgavetype = Oppgavetype.VurderHenvendelse,
+                        fristFerdigstillelse = LocalDate.now(),
+                        beskrivelse = "Saken ligger i ny løsning. Opprettet som følge av personhendelse",
+                        enhetsnummer = null,
+                        behandlingstema = Behandlingstema.Overgangsstønad.value,
+                        tilordnetRessurs = null,
+                        behandlesAvApplikasjon = "familie-ef-sak"
+                    )
+                val oppgaveId = oppgaveClient.opprettOppgave(opprettOppgaveRequest)
+                secureLogger.info("Oppgave opprettet med oppgaveId: $oppgaveId")
+                lagdOppgave = true
+            }
         }
         // er personen stønadsmottaker: opprett oppgave
         // sjekk om foreldre er stønadsmottaker: Opprett oppgave
