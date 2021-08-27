87099a19efa128d70e505671bf9e592c826bc706
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-08-27 16:13:44 +0200

    Bruke felles httpclient i azure-kall

--- src/main/kotlin/no/nav/familie/ef/personhendelse/client/AzureClient.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/client/AzureClient.kt
@@ -1,6 +1,8 @@
 package no.nav.familie.ef.personhendelse.client
 
 import no.nav.familie.ef.personhendelse.common.Token
+import no.nav.familie.http.client.AbstractRestClient
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -8,19 +10,21 @@ import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
+import org.springframework.web.client.RestOperations
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.postForEntity
 
 
 @Component
 class AzureClient(
+    @Qualifier("azure") restOperations: RestOperations,
     @Value("\${AZURE_OPENID_CONFIG_TOKEN_ENDPOINT}")
     private val url: String,
     @Value("\${AZURE_APP_CLIENT_ID}")
     private val clientId: String,
     @Value("\${AZURE_APP_CLIENT_SECRET}")
     private val clientSecret: String,
-) {
+) : AbstractRestClient(restOperations, "azure") {
 
     private var cachedToken: Token? = null
 
