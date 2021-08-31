a80504217758bfe081d767497be0bbee66d07585
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-08-31 13:02:42 +0200

    Kaller på PDL mer som det gjøres i ef-sak for å få det til å bli likt med headers, som blant annet callid og consumerid

---
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/client/pdl/PdlClient.kt
@@ -0,0 +1,79 @@
+package no.nav.familie.ef.personhendelse.client
+
+import com.expediagroup.graphql.client.spring.GraphQLWebClient
+import com.expediagroup.graphql.client.types.GraphQLClientResponse
+import kotlinx.coroutines.runBlocking
+import no.nav.familie.ef.personhendelse.client.pdl.PdlPersonRequest
+import no.nav.familie.ef.personhendelse.client.pdl.PdlPersonRequestVariables
+import no.nav.familie.ef.personhendelse.client.pdl.PdlResponse
+import no.nav.familie.ef.personhendelse.client.pdl.feilsjekkOgReturnerData
+import no.nav.familie.ef.personhendelse.generated.HentPerson
+import no.nav.familie.ef.personhendelse.generated.hentperson.Person
+import no.nav.familie.http.client.AbstractRestClient
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestOperations
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
+
+@Component
+class PdlClient(
+    @Qualifier("azure") restOperations: RestOperations,
+    val azureClient: AzureClient,
+    @Value("\${PDL_URL}")
+    val url: URI,
+    @Value("\${PDL_SCOPE}")
+    val scope: String
+) : AbstractRestClient(restOperations, "pdl") {
+
+    val pathGraphql = "graphql"
+    val pdlUri: URI = UriComponentsBuilder.fromUri(url).pathSegment(pathGraphql).build().toUri()
+
+    fun hentPerson(fnr: String): Person {
+
+        val variables = HentPerson.Variables(fnr, true, true)
+        val pdlPersonRequest = PdlPersonRequest(
+            variables = PdlPersonRequestVariables(fnr),
+            query = HentPerson(variables).query
+        )
+
+        val pdlResponse: PdlResponse<HentPerson.Result> = postForEntity(pdlUri, pdlPersonRequest, httpHeadersPdl())
+        return feilsjekkOgReturnerData(fnr, pdlResponse) { it.hentPerson }
+    }
+
+    fun hentPerson(fnr: String, callId: String): GraphQLClientResponse<HentPerson.Result> {
+
+        val variables = HentPerson.Variables(fnr, true, true)
+        val hentPersonQuery = HentPerson(variables)
+
+        val token = azureClient.hentToken(scope)
+        val client = GraphQLWebClient(
+            url = url.toString(),
+            builder = WebClient.builder().defaultHeaders {
+                it.setBearerAuth(token)
+                it.add("Tema", "ENF")
+            }
+        )
+        return runBlocking { client.execute(hentPersonQuery) }
+    }
+}
+
+private fun httpHeadersPdl(): HttpHeaders {
+
+    return HttpHeaders().apply {
+        add("Tema", "ENF")
+    }
+}
+
+data class PdlError(
+    val message: String,
+    val extensions: PdlExtensions?
+)
+
+data class PdlExtensions(val code: String?) {
+
+    fun notFound() = code == "not_found"
+}
---
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/client/pdl/PdlResponse.kt
@@ -0,0 +1,15 @@
+package no.nav.familie.ef.personhendelse.client.pdl
+
+import no.nav.familie.ef.personhendelse.client.PdlError
+
+data class PdlResponse<T>(val data: T,
+                          val errors: List<PdlError>?) {
+
+    fun harFeil(): Boolean {
+        return errors != null && errors.isNotEmpty()
+    }
+
+    fun errorMessages(): String {
+        return errors?.joinToString { it -> it.message } ?: ""
+    }
+}
