9f7765b5e1357cd952ee1a407d6f644b026eb034
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-09-01 00:01:05 +0200

    Add enableKafka annotation

--- src/main/kotlin/no/nav/familie/ef/personhendelse/configuration/KafkaConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/configuration/KafkaConfig.kt
@@ -5,11 +5,11 @@ import no.nav.person.pdl.leesah.Personhendelse
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
-import org.springframework.kafka.listener.ContainerProperties
-import java.time.Duration
 
+@EnableKafka
 @Configuration
 class KafkaConfig {
 
