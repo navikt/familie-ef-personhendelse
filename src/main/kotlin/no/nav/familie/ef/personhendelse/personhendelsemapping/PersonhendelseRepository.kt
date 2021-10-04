e0c403b0e89af8f35d26fe21c12000d4dabb85e3
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2021-10-04 23:36:09 +0200

    Gjør hendelsestype av hentHendelse non-nullable

--- src/main/kotlin/no/nav/familie/ef/personhendelse/personhendelsemapping/PersonhendelseRepository.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/personhendelsemapping/PersonhendelseRepository.kt
@@ -8,7 +8,7 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
 import java.sql.ResultSet
-import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.UUID
 
 @Repository
@@ -19,23 +19,27 @@ class PersonhendelseRepository(val namedParameterJdbcTemplate: NamedParameterJdb
     fun lagrePersonhendelse(hendelsesId: UUID, oppgaveId: Long, endringstype: Endringstype) {
         val sql = "INSERT INTO hendelse VALUES(:hendelsesId, :oppgaveId, :endringsType, :timestamp)"
         val params = MapSqlParameterSource(
-                mapOf("hendelsesId" to hendelsesId,
-                      "oppgaveId" to oppgaveId,
-                      "endringsType" to endringstype.name,
-                      "timestamp" to LocalDate.now())
+            mapOf(
+                "hendelsesId" to hendelsesId,
+                "oppgaveId" to oppgaveId,
+                "endringsType" to endringstype.name,
+                "timestamp" to LocalDateTime.now()
+            )
         )
         namedParameterJdbcTemplate.update(sql, params)
     }
 
-    fun hentHendelse(hendelsesId: UUID): Hendelse? {
+    fun hentHendelse(hendelsesId: UUID): Hendelse {
         val sql = "SELECT * FROM hendelse WHERE hendelse_id = :hendelsesId"
         val mapSqlParameterSource = MapSqlParameterSource("hendelsesId", hendelsesId)
         return namedParameterJdbcTemplate.queryForObject(sql, mapSqlParameterSource) { rs: ResultSet, _: Int ->
-            Hendelse(UUID.fromString(rs.getString("hendelse_id")),
-                     rs.getLong("oppgave_id"),
-                     Endringstype.valueOf(rs.getString("endringstype")),
-                     rs.getTime("opprettet_tid").toLocalTime())
-        }
+            Hendelse(
+                UUID.fromString(rs.getString("hendelse_id")),
+                rs.getLong("oppgave_id"),
+                Endringstype.valueOf(rs.getString("endringstype")),
+                rs.getTimestamp("opprettet_tid").toLocalDateTime()
+            )
+        }!!
     }
 
 }
