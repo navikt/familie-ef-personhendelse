579e77770e076ae5d3fde2b959da5788d5b66b81
Trond-L <56258085+throndi@users.noreply.github.com>
2021-10-04 12:33:42 +0200

    Update src/main/resources/db/migration/V1__create_hendelse.sql
    Co-authored-by: Johan Blomgren <blommish@users.noreply.github.com>

--- src/main/resources/db/migration/V1__create_hendelse.sql
+++ src/main/resources/db/migration/V1__create_hendelse.sql
@@ -1,5 +1,5 @@
 CREATE TABLE hendelse (
-    hendelse_id UUID NOT NULL,
+    hendelse_id UUID PRIMARY KEY,
     oppgave_id BIGINT,
     endringstype VARCHAR,
     opprettet_tid TIMESTAMP(3) DEFAULT LOCALTIMESTAMP
