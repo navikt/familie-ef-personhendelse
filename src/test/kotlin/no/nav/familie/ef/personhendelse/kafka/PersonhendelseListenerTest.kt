17cbbedb981b68298b6a0b84f67cfb59cacd6fb8
Johan Blomgren <blommish@users.noreply.github.com>
2021-10-13 08:39:51 +0200

    Handlers for personhendelser må mappes på hendelsetypen til enumen og ikke name, då det er hendelsetypen som er opplysningstypen på personhendelsen (#30)

---
+++ src/test/kotlin/no/nav/familie/ef/personhendelse/kafka/PersonhendelseListenerTest.kt
@@ -0,0 +1,75 @@
+package no.nav.familie.ef.personhendelse.kafka
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verify
+import no.nav.familie.ef.personhendelse.client.OppgaveClient
+import no.nav.familie.ef.personhendelse.client.SakClient
+import no.nav.familie.ef.personhendelse.handler.PersonhendelseHandler
+import no.nav.familie.ef.personhendelse.handler.PersonhendelseType
+import no.nav.familie.ef.personhendelse.personhendelsemapping.PersonhendelseRepository
+import no.nav.person.pdl.leesah.Endringstype
+import no.nav.person.pdl.leesah.Personhendelse
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class PersonhendelseListenerTest {
+
+    private val sakClient = mockk<SakClient>()
+    private val oppgaveClient = mockk<OppgaveClient>()
+    private val personhendelseRepository = mockk<PersonhendelseRepository>()
+
+    private lateinit var handler: PersonhendelseHandler
+    private lateinit var listener: PersonhendelseListener
+
+    private val personMedSak = "ident_med_sak"
+    private val personUtenSak = "ident_uten_sak"
+
+    @BeforeEach
+    internal fun setUp() {
+        handler = lagHandler()
+        listener = PersonhendelseListener(listOf(handler), "")
+        every { sakClient.finnesBehandlingForPerson(personMedSak, any()) } returns true
+        every { sakClient.finnesBehandlingForPerson(personUtenSak, any()) } returns false
+    }
+
+    @Test
+    internal fun `skal kalle på handler for hendelse som har handler`() {
+        listener.listen(lagPersonhendelse(personIdent = personUtenSak))
+
+        verify(exactly = 1) { handler.handle(any()) }
+    }
+
+    @Test
+    internal fun `skal ikke kalle på handler for hendelse som ikke er mappet med handler`() {
+        val personhendelse = lagPersonhendelse()
+        personhendelse.opplysningstype = "IKKE_MAPPET"
+
+        listener.listen(personhendelse)
+
+        verify(exactly = 0) { handler.handle(any()) }
+    }
+
+    private fun lagPersonhendelse(
+        endringstype: Endringstype = Endringstype.OPPRETTET,
+        personIdent: String = this.personMedSak,
+        opplysningstype: PersonhendelseType = PersonhendelseType.SIVILSTAND
+    ): Personhendelse {
+        val personhendelse = Personhendelse()
+        personhendelse.personidenter = listOf(personIdent)
+        personhendelse.endringstype = endringstype
+        personhendelse.opplysningstype = opplysningstype.hendelsetype
+        return personhendelse
+    }
+
+    private fun lagHandler() =
+        spyk<PersonhendelseHandler>(object : PersonhendelseHandler(sakClient, oppgaveClient, personhendelseRepository) {
+            override val type: PersonhendelseType = PersonhendelseType.SIVILSTAND
+
+            override fun lagOppgaveBeskrivelse(personhendelse: Personhendelse): String {
+                return "beskrivelse"
+            }
+
+        })
+}
