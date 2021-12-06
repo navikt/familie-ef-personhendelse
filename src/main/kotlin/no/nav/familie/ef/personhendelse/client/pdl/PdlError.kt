74033ab7174ba08c22b6ea343e049003c7ef26e2
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-12-06 09:04:35 +0100

    En test hvor linter ikke er kjørt

--- src/main/kotlin/no/nav/familie/ef/personhendelse/client/pdl/PdlError.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/client/pdl/PdlError.kt
@@ -1,8 +1,7 @@
 package no.nav.familie.ef.personhendelse.client.pdl
 
-data class PdlError(
-    val message: String,
-    val extensions: PdlExtensions?
+data class PdlError(val message: String,
+                    val extensions: PdlExtensions?
 )
 
 data class PdlExtensions(val code: String?) {
