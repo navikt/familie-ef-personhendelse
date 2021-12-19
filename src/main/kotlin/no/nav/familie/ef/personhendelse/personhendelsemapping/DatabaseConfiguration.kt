16dd693eaba720489d9e551fd0246b8a9ce0b328
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-12-19 14:33:13 +0100

    Linter endrer identering i sql-filer og endrer dermed checksum i flyway_schema_history. Kjører repair for å fikse dette.

--- src/main/kotlin/no/nav/familie/ef/personhendelse/personhendelsemapping/DatabaseConfiguration.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/personhendelsemapping/DatabaseConfiguration.kt
@@ -1,9 +1,12 @@
 package no.nav.familie.ef.personhendelse.personhendelsemapping
 
+import org.flywaydb.core.Flyway
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import javax.sql.DataSource
+
 
 @Configuration
 class DatabaseConfiguration {
@@ -11,5 +14,13 @@ class DatabaseConfiguration {
     @Bean
     fun namedParameterJdbcTemplate(dataSource: DataSource): NamedParameterJdbcTemplate {
         return NamedParameterJdbcTemplate(dataSource)
+    }
+
+    @Bean
+    fun cleanMigrateStrategy(): FlywayMigrationStrategy? {
+        return FlywayMigrationStrategy { flyway: Flyway ->
+            flyway.repair()
+            flyway.migrate()
+        }
     }
 }
