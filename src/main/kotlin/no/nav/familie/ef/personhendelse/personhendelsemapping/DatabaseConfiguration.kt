fab5769e2b1193a7c066506373a1bfabbbccb7c1
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-12-19 14:51:00 +0100

    La til familie ef proxy url i config

--- src/main/kotlin/no/nav/familie/ef/personhendelse/personhendelsemapping/DatabaseConfiguration.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/personhendelsemapping/DatabaseConfiguration.kt
@@ -1,12 +1,9 @@
 package no.nav.familie.ef.personhendelse.personhendelsemapping
 
-import org.flywaydb.core.Flyway
-import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import javax.sql.DataSource
-
 
 @Configuration
 class DatabaseConfiguration {
@@ -14,13 +11,5 @@ class DatabaseConfiguration {
     @Bean
     fun namedParameterJdbcTemplate(dataSource: DataSource): NamedParameterJdbcTemplate {
         return NamedParameterJdbcTemplate(dataSource)
-    }
-
-    @Bean
-    fun cleanMigrateStrategy(): FlywayMigrationStrategy? {
-        return FlywayMigrationStrategy { flyway: Flyway ->
-            flyway.repair()
-            flyway.migrate()
-        }
     }
 }
