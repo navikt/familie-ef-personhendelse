c79c866fe16328c6af19f417fa0ba4714f53fc71
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-12-21 13:34:05 +0100

    Endre kolonne-type

--- src/main/resources/db/migration/V3__create_efvedtakhendelse.sql
+++ src/main/resources/db/migration/V3__create_efvedtakhendelse.sql
@@ -4,3 +4,5 @@ CREATE TABLE efvedtakhendelse
     person_ident  VARCHAR NOT NULL,
     stonadstype   VARCHAR NOT NULL
 );
+
+alter table efvedtakhendelse alter column behandling_id type bigint using behandling_id::bigint;
