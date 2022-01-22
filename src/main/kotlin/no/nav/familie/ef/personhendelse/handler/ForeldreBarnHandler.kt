dad850888868771be3cd1fbec83a76ac013f3a26
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2022-01-22 13:48:41 +0100

    Forelder-barn handler for å opprette oppgaver for forelder-barn hendelser der antall barn i PDL er forskjellig fra antall barn i ef-sak

--- src/main/kotlin/no/nav/familie/ef/personhendelse/handler/ForeldreBarnHandler.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/handler/ForeldreBarnHandler.kt
@@ -1,14 +1,25 @@
 package no.nav.familie.ef.personhendelse.handler
 
+import no.nav.familie.ef.personhendelse.client.SakClient
+import no.nav.familie.ef.personhendelse.client.pdl.PdlClient
+import no.nav.familie.ef.personhendelse.generated.enums.ForelderBarnRelasjonRolle
+import no.nav.familie.ef.personhendelse.util.identerUtenAktørId
 import no.nav.person.pdl.leesah.Personhendelse
 
-class NyttBarnHandler : PersonhendelseHandler {
+class ForeldreBarnHandler(val sakClient: SakClient, val pdlClient: PdlClient) : PersonhendelseHandler {
 
-    override val type: PersonhendelseType
-        get() = TODO("Not yet implemented")
+    override val type = PersonhendelseType.FORELDERBARNRELASJON
 
     override fun lagOppgaveBeskrivelse(personhendelse: Personhendelse): String {
-        TODO("Not yet implemented")
+        return "Bruker har fått et nytt barn"
+    }
+
+    override fun skalOppretteOppgave(personhendelse: Personhendelse): Boolean {
+        val personIdent = personhendelse.identerUtenAktørId().first()
+        val antallBarnPdl =
+                pdlClient.hentPerson(personIdent).forelderBarnRelasjon.filter { it.minRolleForPerson == ForelderBarnRelasjonRolle.BARN }.size
+        val antallBarnSøknad = sakClient.hentFnrForAlleBarn(personIdent).size
+        return antallBarnSøknad != antallBarnPdl
     }
 
 }
