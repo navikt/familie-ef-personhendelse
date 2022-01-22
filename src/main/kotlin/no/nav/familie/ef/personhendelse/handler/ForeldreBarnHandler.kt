ccb1db35a12c700640f04ad87d6f1d1fa678e6f6
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2022-01-22 13:38:55 +0100

    Oppdaterer avro skjema for forelder-barn relasjon

---
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/handler/ForeldreBarnHandler.kt
@@ -0,0 +1,14 @@
+package no.nav.familie.ef.personhendelse.handler
+
+import no.nav.person.pdl.leesah.Personhendelse
+
+class NyttBarnHandler : PersonhendelseHandler {
+
+    override val type: PersonhendelseType
+        get() = TODO("Not yet implemented")
+
+    override fun lagOppgaveBeskrivelse(personhendelse: Personhendelse): String {
+        TODO("Not yet implemented")
+    }
+
+}
