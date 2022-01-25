e5714437c046ca83d6a0e53282e33b0acbb39931
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-01-25 11:15:54 +0100

    Bugfix sql update-statement

--- src/main/resources/db/migration/V4__add_aarmaaned_prosessert_efvedtakhendelse.sql
+++ src/main/resources/db/migration/V4__add_aarmaaned_prosessert_efvedtakhendelse.sql
@@ -1,4 +1,4 @@
 ALTER TABLE efvedtakhendelse ADD COLUMN aar_maaned_prosessert VARCHAR;
 ALTER TABLE efvedtakhendelse ADD COLUMN versjon BIGINT;
-UPDATE efvedtakhendelse SET aar_maaned_prosessert='01-2022';
+UPDATE efvedtakhendelse SET aar_maaned_prosessert='2022-01';
 UPDATE efvedtakhendelse SET versjon=1;
