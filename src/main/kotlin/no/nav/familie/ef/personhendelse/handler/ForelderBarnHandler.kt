a062a27c381e8a2838da67a74e9e41471a3e56eb
Trond Lillesand <56258085+throndi@users.noreply.github.com>
2022-01-30 20:15:14 +0100

    Debug logging

--- src/main/kotlin/no/nav/familie/ef/personhendelse/handler/ForelderBarnHandler.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/handler/ForelderBarnHandler.kt
@@ -4,10 +4,14 @@ import no.nav.familie.ef.personhendelse.client.SakClient
 import no.nav.familie.ef.personhendelse.util.identerUtenAktørId
 import no.nav.familie.kontrakter.felles.PersonIdent
 import no.nav.person.pdl.leesah.Personhendelse
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class ForelderBarnHandler(val sakClient: SakClient) : PersonhendelseHandler {
+
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
     override val type = PersonhendelseType.FORELDERBARNRELASJON
 
@@ -18,6 +22,7 @@ class ForelderBarnHandler(val sakClient: SakClient) : PersonhendelseHandler {
     override fun skalOppretteOppgave(personhendelse: Personhendelse): Boolean {
         val personIdent = personhendelse.identerUtenAktørId().first()
         val nyeBarnForBruker = sakClient.finnNyeBarnForBruker(PersonIdent(personIdent))
+        logger.debug("Nye barn for bruker er ${nyeBarnForBruker.size}, hendelseId : ${personhendelse.hendelseId}")
         return nyeBarnForBruker.isNotEmpty()
     }
 }
