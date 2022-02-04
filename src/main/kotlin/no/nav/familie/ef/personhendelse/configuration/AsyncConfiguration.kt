57bfcae7bd79c228360da22bb9c5af18a4fe41d2
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2022-02-04 11:47:36 +0100

    Gjør inntektsendringer async (#103)
    * Gjør inntektsendringer async
    * Testfix

---
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/configuration/AsyncConfiguration.kt
@@ -0,0 +1,8 @@
+package no.nav.familie.ef.personhendelse.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+@Configuration
+@EnableAsync
+class AsyncConfiguration
--- src/test/kotlin/no/nav/familie/ef/personhendelse/inntekt/VedtakendringerServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ef/personhendelse/inntekt/VedtakendringerServiceTest.kt
@@ -9,6 +9,7 @@ import no.nav.familie.kontrakter.felles.objectMapper
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import java.nio.charset.StandardCharsets
+import java.time.YearMonth
 
 class VedtakendringerServiceTest {
 
@@ -32,7 +33,17 @@ class VedtakendringerServiceTest {
         val json: String = readResource("inntekt/InntekthistorikkLoennsinntektTilOffentligYtelseEksempel.json")
         val inntektshistorikkResponse = objectMapper.readValue<InntektshistorikkResponse>(json)
 
-        Assertions.assertThat(vedtakendringer.harNyeVedtak(inntektshistorikkResponse)).isTrue
+        val nyesteArbeidsInntektInformasjonIEksempelJson = inntektshistorikkResponse.inntektForMåned("2021-12")?.first()?.arbeidsInntektInformasjon!!
+        val nestNyesteArbeidsInntektInformasjonIEksempelJson = inntektshistorikkResponse.inntektForMåned("2021-11")?.first()?.arbeidsInntektInformasjon!!
+
+        val oppdatertDatoInntektshistorikkResponse = InntektshistorikkResponse(
+            linkedMapOf(
+                Pair(YearMonth.now().minusMonths(1).toString(), mapOf(Pair("928497704", listOf(InntektVersjon(nyesteArbeidsInntektInformasjonIEksempelJson, null, "innleveringstidspunkt", "opplysningspliktig", 1))))),
+                Pair(YearMonth.now().minusMonths(2).toString(), mapOf(Pair("928497704", listOf(InntektVersjon(nestNyesteArbeidsInntektInformasjonIEksempelJson, null, "innleveringstidspunkt", "opplysningspliktig", 1)))))
+            )
+        )
+
+        Assertions.assertThat(vedtakendringer.harNyeVedtak(oppdatertDatoInntektshistorikkResponse)).isTrue
     }
 
     fun readResource(name: String): String {
