e50d5d47d5b439a877702b49c55888a527468e6a
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2022-02-06 21:20:37 +0100

    Vedtakendringer: Grupper flere utbetalinger for samme stønad på samme måned (kan f.eks. være etterutbetalinger) (#104)
    Grupper flere utbetalinger for samme stønad
    Inntektsendringer: Overgangsstønad og uføretrygd skal ikke medberegnes

--- src/main/kotlin/no/nav/familie/ef/personhendelse/inntekt/InntektsendringerService.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/inntekt/InntektsendringerService.kt
@@ -17,7 +17,7 @@ class InntektsendringerService(
         val nyesteRegistrerteInntekt = inntektshistorikkResponse.inntektForMåned(YearMonth.now().minusMonths(1).toString())
         val nestNyesteRegistrerteInntekt = inntektshistorikkResponse.inntektForMåned(YearMonth.now().minusMonths(2).toString())
 
-        return harMottattMerIOffentligeYtelser(nestNyesteRegistrerteInntekt, nyesteRegistrerteInntekt) || har10ProsentHøyereInntektEnnForventet(nyesteRegistrerteInntekt, identMedForventetInntekt)
+        return har10ProsentHøyereInntektEnnForventet(nyesteRegistrerteInntekt, identMedForventetInntekt)
     }
 
     private fun har10ProsentHøyereInntektEnnForventet(nyesteRegistrerteInntekt: List<InntektVersjon>?, identMedForventetInntekt: Map.Entry<String, Int?>): Boolean {
@@ -36,6 +36,7 @@ class InntektsendringerService(
         return samletInntekt >= (månedligForventetInntekt * 1.1) && samletInntekt > 0 // Må sjekke om samletInntekt er større enn 0 for å ikke få true dersom alle variabler er 0 (antakelig kun i test)
     }
 
+    // Denne kan være vanskelig å bruke, da det kan være mye etterbetalinger som gir falsk positiv
     private fun harMottattMerIOffentligeYtelser(nestNyesteRegistrerteInntekt: List<InntektVersjon>?, nyesteRegistrerteInntekt: List<InntektVersjon>?): Boolean {
         val sumOffentligeYtelserForNyeste = sumOffentligeYtelser(nyesteRegistrerteInntekt)
         val sumOffentligeYtelserForNestNyeste = sumOffentligeYtelser(nestNyesteRegistrerteInntekt)
@@ -48,11 +49,11 @@ class InntektsendringerService(
 
         val inntektListe = nyesteRegistrerteInntekt?.firstOrNull { it.versjon == nyesteVersjon }?.arbeidsInntektInformasjon?.inntektListe
         val beløpYtelseFraOffentligList = inntektListe?.filter {
-            it.inntektType == InntektType.YTELSE_FRA_OFFENTLIGE &&
-                it.beskrivelse != "overgangsstoenadTilEnsligMorEllerFarSomBegynteAaLoepe1April2014EllerSenere" &&
-                it.beskrivelse != "arbeidsavklaringspenger" &&
-                it.beskrivelse != "dagpengerVedArbeidsloeshet"
+            it.inntektType == InntektType.YTELSE_FRA_OFFENTLIGE && !ignorerteYtelserIBeregningAvInntekt.contains(it.beskrivelse)
         }?.map { it.beløp } ?: listOf() // Sjekker kun mot faste månedlige utbetalinger (ikke dagpenger / AAP fordi de ofte er variable)
         return beløpYtelseFraOffentligList.sumOf { it }
     }
+
+    // Ignorterte ytelser: AAP og Dagpenger er ignorert fordi de er variable. Alle uføre går under annet regelverk (samordning) og skal derfor ignoreres.
+    val ignorerteYtelserIBeregningAvInntekt = listOf("overgangsstoenadTilEnsligMorEllerFarSomBegynteAaLoepe1April2014EllerSenere", "arbeidsavklaringspenger", "dagpengerVedArbeidsloeshet", "ufoeretrygd", "ufoereytelseEtteroppgjoer")
 }
