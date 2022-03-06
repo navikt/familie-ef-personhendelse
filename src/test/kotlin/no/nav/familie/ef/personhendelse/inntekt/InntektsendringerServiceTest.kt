31f2bb077874b5d4d564b9c489644cea19b6e0fa
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-03-06 21:33:27 +0100

    Bump token-validation-spring-test & mockk

--- src/test/kotlin/no/nav/familie/ef/personhendelse/inntekt/InntektsendringerServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ef/personhendelse/inntekt/InntektsendringerServiceTest.kt
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.nio.charset.StandardCharsets
 import java.time.YearMonth
-import java.util.*
+import java.util.AbstractMap
 
 class InntektsendringerServiceTest {
 
