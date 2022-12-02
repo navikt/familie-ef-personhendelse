8248527f83c416b4922ce3c54073d1218726f5bf
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2022-12-02 09:33:17 +0100

    Definerer eksplisitt dei query-params som faktisk blir sendt inn for finnMappeRequest (#299)
    Tilretteleggjing for å fjerne toQueryParams-metoden, som fører med seg mykje unødig kompleksitet. Send inn parametere eksplisitt fremfor å bruke FinnMappeRequest objektet
    
    Co-authored-by: Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>

--- src/main/kotlin/no/nav/familie/ef/personhendelse/client/OppgaveClient.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/client/OppgaveClient.kt
@@ -5,7 +5,6 @@ import no.nav.familie.kontrakter.felles.Behandlingstema
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.Tema
 import no.nav.familie.kontrakter.felles.getDataOrThrow
-import no.nav.familie.kontrakter.felles.oppgave.FinnMappeRequest
 import no.nav.familie.kontrakter.felles.oppgave.FinnMappeResponseDto
 import no.nav.familie.kontrakter.felles.oppgave.IdentGruppe
 import no.nav.familie.kontrakter.felles.oppgave.Oppgave
@@ -52,10 +51,11 @@ class OppgaveClient(
         return response.getDataOrThrow()
     }
 
-    fun finnMapper(finnMappeRequest: FinnMappeRequest): FinnMappeResponseDto {
+    fun finnMapper(enhetsnummer: String, limit: Int = 1000): FinnMappeResponseDto {
         val response = getForEntity<Ressurs<FinnMappeResponseDto>>(
             UriComponentsBuilder.fromUri(URI.create("$oppgaveUrl/mappe/sok"))
-                .queryParams(finnMappeRequest.toQueryParams())
+                .queryParam("enhetsnr", enhetsnummer)
+                .queryParam("limit", limit)
                 .build()
                 .toUri()
         )
