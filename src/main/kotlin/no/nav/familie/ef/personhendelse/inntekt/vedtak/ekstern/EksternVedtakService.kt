c1b693d9f50f126b8f828f1bdd3f530cdf2623e8
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2023-02-22 14:28:57 +0100

    Lytt til eksterne vedtak topic som inneholder vedtak fra sykepenger og foreldrepenger. (#343)
    Ignorer sykepenger og foreldrepenger i månedlig kjøring.

---
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/inntekt/vedtak/ekstern/EksternVedtakService.kt
@@ -0,0 +1,14 @@
+package no.nav.familie.ef.personhendelse.inntekt.vedtak.ekstern
+
+import no.nav.familie.ef.personhendelse.client.SakClient
+import org.springframework.stereotype.Service
+
+@Service
+class EksternVedtakService(
+    val sakClient: SakClient,
+) {
+
+    fun mottarEfStønad(vedtakhendelse: Vedtakhendelse): Boolean {
+        return sakClient.harLøpendeStønad(setOf(vedtakhendelse.fødselsnummer))
+    }
+}
