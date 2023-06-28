e19c32cf2e4a3077ce6a1bc6b5e9f01554683681
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2023-06-28 09:45:43 +0200

    Log opprettede oppgaver

---
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/util/OppgaveController.kt
@@ -0,0 +1,28 @@
+package no.nav.familie.ef.personhendelse.util
+
+import no.nav.familie.ef.personhendelse.client.OppgaveClient
+import no.nav.familie.ef.personhendelse.client.pdl.secureLogger
+import no.nav.familie.ef.personhendelse.personhendelsemapping.PersonhendelseRepository
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(path = ["/api/oppgave"])
+@ProtectedWithClaims(issuer = "azuread")
+class OppgaveController(val oppgaveClient: OppgaveClient, val personhendelseRepository: PersonhendelseRepository) {
+
+    @PostMapping("/logOpprettedeOppgaver")
+    fun logOpprettedeOppgaver() {
+        val oppgaveIds = personhendelseRepository.hentAlleOppgaveIds()
+        oppgaveIds?.forEach {
+            val oppgave = oppgaveClient.finnOppgaveMedId(it)
+            secureLogger.info(
+                "oppgave opprettet fra livshendelse: " + oppgave.beskrivelse +
+                    ";status: ${oppgave.status};opprettetTidspunkt: ${oppgave.opprettetTidspunkt}" +
+                    ";aktørId" + oppgave.aktoerId,
+            )
+        }
+    }
+}
