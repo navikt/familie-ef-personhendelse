7cde915218ed8675b0094566e93d10cf02a31423
Trondl <56258085+throndi@users.noreply.github.com>
2023-09-11 11:11:31 +0200

    Vurderkonsekvensoppgave for kontantstøttevedtak  (#418)
    * Skal lytte på vedtakshendelser fra kontantstøtte og opprette oppgave av typen VurderKonsekvensForYtelse hvis det finnes løpende barnetilsyn
    
    * Setter seekToEnd

---
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/kontantstøtte/vedtak/KontantstøtteVedtakListener.kt
@@ -0,0 +1,67 @@
+package no.nav.familie.ef.personhendelse.kontantstøtte.vedtak
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.familie.eksterne.kontrakter.VedtakDVH
+import no.nav.familie.kontrakter.felles.objectMapper
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.listener.ConsumerSeekAware
+import org.springframework.stereotype.Component
+
+@Component
+class KontantstøtteVedtakListener(val kontantstøtteVedtakService: KontantstøtteVedtakService) : ConsumerSeekAware {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+    private val securelogger = LoggerFactory.getLogger("secureLogger")
+    private val vurderKonsekvensOppgaveBeskrivelse =
+        "Bruker har løpende stønad til barnetilsyn og har fått innvilget kontantstøtte."
+
+    @KafkaListener(
+        id = "aapen-kontantstotte-vedtak-aiven",
+        groupId = "familie-ef-kontantstøtte-vedtak",
+        topics = ["\${FAMILIE_KS_VEDTAK_TOPIC}"],
+        containerFactory = "kafkaKontantstøtteVedtakListenerContainerFactory",
+    )
+    fun listen(consumerRecord: ConsumerRecord<String, String>) {
+        val vedtakhendelse = objectMapper.readValue<VedtakDVH>(consumerRecord.value())
+        try {
+            logger.info("Leser vedtak for kontantstøtte med behandlingId: ${vedtakhendelse.behandlingsId}")
+            opprettOppgaveHvisPersonHarLøpendeBarnetilsyn(vedtakhendelse.person.personIdent)
+        } catch (e: Exception) {
+            logger.error("Feil ved håndtering av kontantstøttevedtak - se securelogs for mer detaljer")
+            securelogger.error(
+                "Feil ved håndtering av vedtakhendelse med behandlingsId ${vedtakhendelse.behandlingsId} med ytelse " +
+                    "kontantstøtte : ${e.message} hendelse={}",
+                objectMapper.writeValueAsString(vedtakhendelse),
+                e,
+            )
+            throw e
+        }
+    }
+
+    private fun opprettOppgaveHvisPersonHarLøpendeBarnetilsyn(personIdent: String) {
+        if (kontantstøtteVedtakService.harLøpendeBarnetilsyn(personIdent)) {
+            kontantstøtteVedtakService.opprettVurderKonsekvensOppgaveForBarnetilsyn(
+                personIdent = personIdent,
+                vurderKonsekvensOppgaveBeskrivelse,
+            )
+        }
+    }
+
+    /**
+     * TODO : Kommenter ut / slett etter første deploy og deploy på nytt for å ikke trigge seekToEnd igjen senere.
+     */
+    override fun onPartitionsAssigned(
+        assignments: MutableMap<org.apache.kafka.common.TopicPartition, Long>,
+        callback: ConsumerSeekAware.ConsumerSeekCallback,
+    ) {
+        logger.info("overrided onPartitionsAssigned seekToBeginning")
+        assignments.keys.stream()
+            .filter { it.topic() == "teamfamilie.aapen-kontantstotte-vedtak-v1" }
+            .forEach {
+                //callback.seekToBeginning("teamfamilie.aapen-kontantstotte-vedtak-v1", it.partition())
+                 callback.seekToEnd("teamfamilie.aapen-kontantstotte-vedtak-v1", it.partition())
+            }
+    }
+}
---
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/kontantstøtte/vedtak/KontantstøtteVedtakService.kt
@@ -0,0 +1,27 @@
+package no.nav.familie.ef.personhendelse.kontantstøtte.vedtak
+
+import no.nav.familie.ef.personhendelse.client.OppgaveClient
+import no.nav.familie.ef.personhendelse.client.SakClient
+import no.nav.familie.ef.personhendelse.client.lagVurderKonsekvensoppgaveForBarnetilsyn
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class KontantstøtteVedtakService(val efSakClient: SakClient, val oppgaveClient: OppgaveClient) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    fun harLøpendeBarnetilsyn(personIdent: String): Boolean {
+        return efSakClient.harLøpendeBarnetilsyn(personIdent)
+    }
+
+    fun opprettVurderKonsekvensOppgaveForBarnetilsyn(personIdent: String, beskrivelse: String) {
+        val opprettOppgaveRequest = lagVurderKonsekvensoppgaveForBarnetilsyn(
+            personIdent = personIdent,
+            beskrivelse = beskrivelse,
+        )
+        val oppgaveId = oppgaveClient.opprettOppgave(opprettOppgaveRequest)
+        oppgaveClient.leggOppgaveIMappe(oppgaveId)
+        logger.info("Oppgave for kontantstøttevedtak opprettet med oppgaveId=$oppgaveId")
+    }
+}
