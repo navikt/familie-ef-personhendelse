58ef5625c88c3fa969299c3bb3c4f490be59cf82
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2024-01-02 15:50:30 +0100

    Lager en egen cron expression som er forskjellig fra dev og prod

--- src/main/kotlin/no/nav/familie/ef/personhendelse/inntekt/InntektsendringerScheduler.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/inntekt/InntektsendringerScheduler.kt
@@ -1,15 +1,20 @@
 package no.nav.familie.ef.personhendelse.inntekt
 
 import no.nav.familie.leader.LeaderClient
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 @Service
 class InntektsendringerScheduler(val vedtakendringerService: VedtakendringerService) {
 
-    @Scheduled(cron = "0 0 4 6 * *") // kl 04:00 den 6. hver måned
+    val logger: Logger = LoggerFactory.getLogger(this::class.java)
+    @Scheduled(cron = "\${INNTEKTSKONTROLL_CRON_EXPRESSION}") // kl 04:00 den 6. hver måned
     fun inntektskontroll() {
+        logger.info("Cron scheduler starter inntektskontroll")
         if (LeaderClient.isLeader() == true) {
+            logger.info("Leader-pod starter inntektskontroll")
             vedtakendringerService.beregnInntektsendringerOgLagreIDb()
             vedtakendringerService.opprettOppgaverForInntektsendringer(true)
         }
