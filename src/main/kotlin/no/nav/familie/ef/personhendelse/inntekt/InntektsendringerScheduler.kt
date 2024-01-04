922be384c92521a38f068664bb29c2fafaa1f333
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2024-01-04 14:51:33 +0100

    Schedule inntektskontroll (#477)
    * Lager en egen cron expression som er forskjellig fra dev og prod
    * Fjerner leader-pod sjekk
    * Trenger ikke async-flagg da dette trigges fra scheduler og ikke rest-kall

--- src/main/kotlin/no/nav/familie/ef/personhendelse/inntekt/InntektsendringerScheduler.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/inntekt/InntektsendringerScheduler.kt
@@ -1,17 +1,19 @@
 package no.nav.familie.ef.personhendelse.inntekt
 
 import no.nav.familie.leader.LeaderClient
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 @Service
 class InntektsendringerScheduler(val vedtakendringerService: VedtakendringerService) {
 
-    @Scheduled(cron = "0 0 4 6 * *") // kl 04:00 den 6. hver måned
+    val logger: Logger = LoggerFactory.getLogger(this::class.java)
+    @Scheduled(cron = "\${INNTEKTSKONTROLL_CRON_EXPRESSION}") // kl 04:00 den 6. hver måned
     fun inntektskontroll() {
-        if (LeaderClient.isLeader() == true) {
-            vedtakendringerService.beregnInntektsendringerOgLagreIDb()
-            vedtakendringerService.opprettOppgaverForInntektsendringer(true)
-        }
+        logger.info("Cron scheduler starter inntektskontroll")
+        vedtakendringerService.beregnInntektsendringerOgLagreIDb()
+        vedtakendringerService.opprettOppgaverForInntektsendringer(true)
     }
 }
