efb43689c20e329cef0d36c646b448f884263275
krikof-nav <kristian.kofoed@nav.no>
2025-03-05 10:35:07 +0100

    Endret scheduled annotering for inntektskontroll til å kjøre ved oppstart for testing.

--- src/main/kotlin/no/nav/familie/ef/personhendelse/inntekt/InntektsendringerScheduler.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/inntekt/InntektsendringerScheduler.kt
@@ -11,7 +11,8 @@ class InntektsendringerScheduler(
 ) {
     val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
-    @Scheduled(cron = "\${INNTEKTSKONTROLL_CRON_EXPRESSION}") // kl 04:00 den 6. hver måned
+    @Scheduled(initialDelay = 60 * 1000L, fixedDelay = 365 * 24 * 60 * 60 * 1000L) // Kjører ved oppstart av app
+    // @Scheduled(cron = "\${INNTEKTSKONTROLL_CRON_EXPRESSION}") // kl 04:00 den 6. hver måned // TODO: Ikke glem å sette meg tilbake.
     fun inntektskontroll() {
         logger.info("Cron scheduler starter inntektskontroll")
         inntektsendringerService.beregnInntektsendringerOgLagreIDb()
