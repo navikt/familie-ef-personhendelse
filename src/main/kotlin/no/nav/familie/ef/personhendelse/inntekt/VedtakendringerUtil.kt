e22aabe83ac7b65834f0ca4bf3589643a1defd2f
krikof-nav <kristian.kofoed@nav.no>
2025-03-10 11:57:27 +0100

    Endringer til offentligeYtelser metode.

--- src/main/kotlin/no/nav/familie/ef/personhendelse/inntekt/VedtakendringerUtil.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/inntekt/VedtakendringerUtil.kt
@@ -6,7 +6,7 @@ import no.nav.familie.ef.personhendelse.inntekt.inntektv2.InntektTypeV2
 import java.time.YearMonth
 
 object VedtakendringerUtil {
-    fun harNyeVedtak(inntektResponse: InntektResponse) = nyeVedtak(inntektResponse).isNotEmpty()
+    fun harNyeVedtak(inntektResponse: InntektResponse): Boolean = nyeVedtak(inntektResponse).isNotEmpty()
 
     fun offentligeYtelserForNyesteMåned(inntektResponse: InntektResponse): List<String> {
         val nyesteRegistrerteInntekt = inntektResponse.månedsData.filter { it.måned == YearMonth.now().minusMonths(1) }
@@ -20,19 +20,27 @@ object VedtakendringerUtil {
         val offentligeYtelserForNyesteMåned = offentligeYtelser(nyesteRegistrerteInntekt)
         val offentligeYtelserForNestNyesteMåned = offentligeYtelser(nestNyesteRegistrerteInntekt)
 
-        return offentligeYtelserForNyesteMåned.minus(offentligeYtelserForNestNyesteMåned)
+        return offentligeYtelserForNyesteMåned.minus(offentligeYtelserForNestNyesteMåned.toSet())
     }
 
-    private fun offentligeYtelser(nyesteRegistrerteInntekt: List<InntektMåned>): List<String> {
-        val offentligYtelseInntekt1 =
-            nyesteRegistrerteInntekt.filter {
-                it.inntektListe.any { offentligYtelse ->
-                    offentligYtelse.type == InntektTypeV2.YTELSE_FRA_OFFENTLIGE && offentligYtelse.beskrivelse != "overgangsstoenadTilEnsligMorEllerFarSomBegynteAaLoepe1April2014EllerSenere"
+    private fun offentligeYtelser(inntektMåneder: List<InntektMåned>): List<String> {
+        val offentligYtelseInntekt =
+            inntektMåneder.filter {
+                it.inntektListe.any { inntekt ->
+                    inntekt.type == InntektTypeV2.YTELSE_FRA_OFFENTLIGE &&
+                        inntekt.beskrivelse != "overgangsstoenadTilEnsligMorEllerFarSomBegynteAaLoepe1April2014EllerSenere"
                 }
             }
 
-        val inntektListe = offentligYtelseInntekt1.firstOrNull()?.inntektListe ?: emptyList()
+        val inntektListe = offentligYtelseInntekt.flatMap { it.inntektListe }
 
-        return inntektListe.filter { it.type == InntektTypeV2.YTELSE_FRA_OFFENTLIGE && it.beskrivelse != "overgangsstoenadTilEnsligMorEllerFarSomBegynteAaLoepe1April2014EllerSenere" && it.tilleggsinformasjon?.type == "ETTERBETALINGSPERIODE" }.groupBy { it.beskrivelse }.map { it.key }
+        return inntektListe
+            .filter {
+                it.type == InntektTypeV2.YTELSE_FRA_OFFENTLIGE &&
+                    it.beskrivelse != "overgangsstoenadTilEnsligMorEllerFarSomBegynteAaLoepe1April2014EllerSenere" &&
+                    it.tilleggsinformasjon?.type != "ETTERBETALINGSPERIODE"
+            }.groupBy { it.beskrivelse }
+            .keys
+            .toList()
     }
 }
