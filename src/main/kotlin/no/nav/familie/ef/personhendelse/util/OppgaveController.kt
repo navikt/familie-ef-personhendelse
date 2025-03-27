4653cc6e5b33b9babfe90742be120de66623bb1e
krikof-nav <kristian.kofoed@nav.no>
2025-03-27 10:40:29 +0100

    Tester om en metode for oppgave controller dukker opp i Swagger. Dette skal fjernes på sikt.

--- src/main/kotlin/no/nav/familie/ef/personhendelse/util/OppgaveController.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/util/OppgaveController.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.ef.personhendelse.util
 
+import io.swagger.v3.oas.annotations.Operation
 import no.nav.familie.ef.personhendelse.client.OppgaveClient
 import no.nav.familie.ef.personhendelse.client.pdl.secureLogger
 import no.nav.familie.ef.personhendelse.personhendelsemapping.PersonhendelseRepository
@@ -16,6 +17,13 @@ class OppgaveController(
     val oppgaveClient: OppgaveClient,
     val personhendelseRepository: PersonhendelseRepository,
 ) {
+    // TODO: Dette skal fjernes.
+    @Operation(
+        description =
+            "Hei, dette er en test!",
+        summary =
+            "Hei, dette er en test!",
+    )
     @PostMapping("/logOpprettedeOppgaver")
     fun logOpprettedeOppgaver() {
         val oppgaveIds = personhendelseRepository.hentAlleOppgaveIds()
