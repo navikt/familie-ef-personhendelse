b3a76030a8192448046665f1c7f743f264d604da
endrehm <endre.heien.mo@nav.no>
2025-07-29 11:31:35 +0200

    Lagt til prosessering og opprettet task for aap endringer og test

--- src/main/kotlin/no/nav/familie/ef/personhendelse/personhendelsemapping/DatabaseConfiguration.kt
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/personhendelsemapping/DatabaseConfiguration.kt
@@ -1,12 +1,23 @@
 package no.nav.familie.ef.personhendelse.personhendelsemapping
 
+import no.nav.familie.prosessering.PropertiesWrapperTilStringConverter
+import no.nav.familie.prosessering.StringTilPropertiesWrapperConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import javax.sql.DataSource
 
 @Configuration
-class DatabaseConfiguration {
+@EnableJdbcRepositories("no.nav.familie")
+class DatabaseConfiguration : AbstractJdbcConfiguration() {
     @Bean
     fun namedParameterJdbcTemplate(dataSource: DataSource): NamedParameterJdbcTemplate = NamedParameterJdbcTemplate(dataSource)
+
+    override fun userConverters(): List<*> =
+        listOf(
+            PropertiesWrapperTilStringConverter(),
+            StringTilPropertiesWrapperConverter(),
+        )
 }
---
+++ src/test/kotlin/no/nav/familie/ef/personhendelse/inntekt/OpprettOppgaverForArbeidsavklaringspengerEndringerTaskTest.kt
@@ -0,0 +1,42 @@
+package no.nav.familie.ef.personhendelse.inntekt
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import no.nav.familie.ef.personhendelse.IntegrasjonSpringRunnerTest
+import no.nav.familie.prosessering.internal.TaskService
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+class OpprettOppgaverForArbeidsavklaringspengerEndringerTaskTest : IntegrasjonSpringRunnerTest() {
+    private val inntektOppgaveService = mockk<InntektOppgaveService>(relaxed = true)
+
+    @Autowired
+    private lateinit var taskService: TaskService
+
+    private lateinit var opprettOppgaverForArbeidsavklaringspengerEndringerTask: OpprettOppgaverForArbeidsavklaringspengerEndringerTask
+
+    @BeforeEach
+    fun setup() {
+        opprettOppgaverForArbeidsavklaringspengerEndringerTask =
+            OpprettOppgaverForArbeidsavklaringspengerEndringerTask(
+                inntektOppgaveService = inntektOppgaveService,
+            )
+    }
+
+    @Test
+    fun `Sjekk at man kan opprette task for arbeidsavklaringspenger endringer og at den har riktig metadata`() {
+        val payload = """{"personIdent":"123", "måned":"6"}"""
+        val task = OpprettOppgaverForArbeidsavklaringspengerEndringerTask.opprettTask(payload)
+        taskService.save(task)
+        val taskList = taskService.findAll()
+        val taskFraDB = taskList.first()
+        assertThat(taskFraDB.metadata).isNotEmpty
+        assertThat(taskFraDB.metadataWrapper.properties.keys.size).isEqualTo(3)
+        assertThat(taskFraDB.metadataWrapper.properties.keys).contains("personIdent", "måned", "callId")
+        opprettOppgaverForArbeidsavklaringspengerEndringerTask.doTask(task)
+        verify(exactly = 1) { inntektOppgaveService.opprettOppgaveForArbeidsavklaringspengerEndring(any(), any()) }
+    }
+}
