74c932cbfaca42ef83addf31493a4c891821564b
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2025-08-07 12:39:16 +0200

    Logg inntekt for person (#651)

---
+++ src/main/kotlin/no/nav/familie/ef/personhendelse/inntekt/LoggInntektForPersonTask.kt
@@ -0,0 +1,43 @@
+package no.nav.familie.ef.personhendelse.inntekt
+
+import no.nav.familie.kontrakter.felles.objectMapper
+import no.nav.familie.prosessering.AsyncTaskStep
+import no.nav.familie.prosessering.TaskStepBeskrivelse
+import no.nav.familie.prosessering.domene.Task
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import java.time.YearMonth
+import java.util.Properties
+
+@Service
+@TaskStepBeskrivelse(
+    taskStepType = LoggInntektForPersonTask.TYPE,
+    maxAntallFeil = 1,
+    settTilManuellOppfølgning = true,
+    beskrivelse = "Logger inntekt for gitt person",
+)
+class LoggInntektForPersonTask(
+    private val inntektClient: InntektClient,
+) : AsyncTaskStep {
+    private val securelogger = LoggerFactory.getLogger("secureLogger")
+
+    override fun doTask(task: Task) {
+        val inntekt = inntektClient.hentInntekt(personIdent = task.payload, YearMonth.now().minusMonths(12), YearMonth.now())
+        securelogger.info("InntektResponse for person ${task.payload}")
+        securelogger.info("${objectMapper.writeValueAsString(inntekt)}")
+    }
+
+    companion object {
+        const val TYPE = "loggInntektForPerson"
+
+        fun opprettTask(payload: String): Task =
+            Task(
+                type = TYPE,
+                payload = payload,
+                properties =
+                    Properties().apply {
+                        this["personIdent"] = payload
+                    },
+            )
+    }
+}
